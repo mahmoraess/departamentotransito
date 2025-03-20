279ed2c82da2b08008141d6a966276c9515231e7
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-03-20 16:07:51 -0400

    Incrementação do formulário e explicação sobre url

--- controllers/veiculoController.js
+++ controllers/veiculoController.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 class VeiculoController{
     static formCadastro(req, res) {
-        res.sendFile(path.join(__dirname, "../", "views", "formCadastro.html"));
+        res.sendFile(path.join(__dirname, "../", "views", "formVeiculo.html"));
     }
 
     static cadastrar(req, res) {}
