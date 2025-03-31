f275807c095a7897517aba79789bb47c5d99a153
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-03-31 17:36:06 -0400

    aula 31/03 prisma

--- controllers/veiculoController.js
+++ controllers/veiculoController.js
@@ -1,12 +1,26 @@
 const path = require("path");
 
+const { PrismaClient } = require("@prisma/client");
+const client = new PrismaClient();
+
 class VeiculoController{
     static formCadastro(req, res) {
         res.sendFile(path.join(__dirname, "../", "views", "formVeiculo.html"));
     }
 
-    static cadastrar(req, res) {
-        res.send(JSON.stringify( req.body ));
+    static async cadastrar(req, res) {
+        res.send(JSON.stringify(req.body));
+
+        const {modelo, placa, ano, cor} = req.body
+
+        const veiculo = await client.veiculo.create({data: {
+            modelo,
+            placa,
+            ano: parseInt(ano),
+            cor
+        }});
+
+        res.send({veiculo});
     }
 
     static buscarTodos(req, res) {}
