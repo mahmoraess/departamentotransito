375de5081b9ab16f65bfa99f1047d76cac1bf22b
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-06-23 19:01:04 -0400

    aula 23/06 - aula login usuario

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { PrismaClient } = require("@prisma/client");
 const client = new PrismaClient();
@@ -22,5 +23,41 @@ class UsuarioController {
         res.json({
             usuarioId: usuario.id,
         });
+
+    }
+
+    static async login(req, res) {
+        const {email, senha} = req.body;
+
+        //Verificar se usuário existe
+        const usuario = await client.usuario.findUnique({
+            where: {
+                email: email,
+            },
+        });
+        if(!usuario){
+            return res.json({
+                msg: "Usuário não encontrado!",
+            });
+        }
+
+        //Verificar se a senha está correta
+        const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha);
+        if (!senhaCorreta) {
+            return res.json({
+                msg: "Senha incorreta!",
+            });
+        }
+
+        //Emitir um token 
+        const token = jwt.sign({ id: usuario.id }, process.env.SENHA_SERVIDOR, {
+            expiresIn: "1h",
+            });  
+            res.json({
+                msg: "Autenticado!",
+                token: token,
+            })
     }
 }
+
+module.exports = UsuarioController;
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const UsuarioController = require("../controllers/UsuarioController");
 
-router.post("./cadastro", UsuarioController.cadastrar);
+router.post("/cadastro", UsuarioController.cadastrar);
+
+router.post("/login", UsuarioController.login)
 
 module.exports = router;
